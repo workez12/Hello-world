a845c64794353b13c397f0b1545e818cef483312
workez12 <wez@almartha.com>
2018-11-24 14:38:39 -0500

    test

---
+++ jenkinsfile.sh
@@ -0,0 +1,33 @@
+pipeline
+   agent any
+
+stages {
+    stage ('compile stage') {
+
+        steps {
+        withMaven('maven_3_5_0') {
+            sh 'mvn clean compile'
+
+            }
+        }
+    }
+    stage (Testing Stage')   {
+        steps {
+        withMaven('maven_3_5_0') {
+            sh 'mvn clean compile'
+      stage ('Testing Stage') {
+          steps {
+        withMaven('maven_3_5_0') {
+            sh 'mvn test'
+        }
+      }
+}
+stage ('Deployment stage')  {
+    steps {
+        withMaven('maven_3_5_0') {
+            sh 'mvn deploy'
+        }
+    }
+  }
+
+}
